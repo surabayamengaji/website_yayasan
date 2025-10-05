ec788db9fb09684672138a4d0274cef5fe30f1a4
itsm <it.surabayamengaji@gmail.com>
2025-10-05 23:40:47 +0700

    Add donation component and related assets
    - Introduced a new donation component with HTML, CSS, and TypeScript files.
    - Added SVG icons for donation categories: graduate, hand-holding, star, and pc.
    - Included new images (keg1.jpg, keg2.jpg, keg3.png, wtp.webp) for the donation section.
    - Updated app structure to include the donation component in the main application.
    - Modified footer styles and index.html to include new font families.
    - Enhanced global styles with new font classes for better typography.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Headers } from './shared/components/headers/headers';
 import { Footers } from './shared/components/footers/footers';
+import { Donation } from './shared/components/donation/donation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footers],
+  imports: [RouterOutlet, Footers, Donation],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/shared/components/donation/donation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation',
+  imports: [],
+  templateUrl: './donation.html',
+  styleUrl: './donation.css'
+})
+export class Donation {
+
+}
